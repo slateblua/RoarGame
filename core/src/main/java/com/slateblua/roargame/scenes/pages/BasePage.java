419cfd141fb56f8772ed81e71149e3d7a185cb10
Sergey Hayriyan <sega1ness@gmail.com>
2025-05-04 02:38:41 +0400

    Update BasePage

--- core/src/main/java/com/slateblua/roargame/scenes/pages/BasePage.java
+++ core/src/main/java/com/slateblua/roargame/scenes/pages/BasePage.java
@@ -1,17 +1,38 @@
 package com.slateblua.roargame.scenes.pages;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.slateblua.roargame.Resources;
 
 public abstract class BasePage extends Table {
+    protected Table content;
+    protected Cell<Table> contentCell;
+
     public BasePage () {
         setFillParent(true);
-        init();
-    }
+        setBackground(Resources.getDrawable("components/ui-white-pixel", Color.valueOf("#69605Bff")));
+        setTouchable(Touchable.enabled);
+        addListener(new ClickListener() {
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
 
-    /**
-     * Initialize the UI components of the page
-     */
-    protected abstract void init ();
+            @Override
+            public void clicked (InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+            }
+        });
+
+        content = new Table();
+        contentCell = add(content).grow();
+        constructContent(content);
+    }
+    protected abstract void constructContent (Table content);
 
     /**
      * Called when the page becomes visible
