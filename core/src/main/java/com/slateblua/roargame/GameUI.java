1c2bbf66aec3d8ca5e35ba413e12911ed222646f
Sergey Hayriyan <sega1ness@gmail.com>
2025-05-11 14:38:42 +0400

    Draft UI

--- core/src/main/java/com/slateblua/roargame/GameUI.java
+++ core/src/main/java/com/slateblua/roargame/GameUI.java
@@ -3,10 +3,14 @@ package com.slateblua.roargame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.slateblua.roargame.scenes.components.IconButton;
+import com.slateblua.roargame.scenes.components.Style;
 import com.slateblua.roargame.scenes.pages.BasePage;
 import com.slateblua.roargame.scenes.pages.BasePopup;
 import com.slateblua.roargame.scenes.pages.NavController;
+import com.slateblua.roargame.weapon.WeaponData;
 import lombok.Getter;
 
 public class GameUI {
@@ -27,13 +31,32 @@ public class GameUI {
     private GameUI () {
         stage = new Stage(new ScreenViewport());
 
+        final Table weaponsTable = createWeaponsComponent();
+
         rootTable = new Table();
         rootTable.setFillParent(true);
+        rootTable.add(weaponsTable).expand().bottom();
+
         stage.addActor(rootTable);
 
         navController = new NavController(stage, rootTable);
 
         Gdx.input.setInputProcessor(stage);
+    }
+
+    private Table createWeaponsComponent () {
+        final Table weaponsTable = new Table();
+        weaponsTable.pad(20);
+        weaponsTable.defaults().space(20).size(150);
+        final Array<WeaponData> weapons = Locator.get(GameData.class).getWeapons();
+
+        for (WeaponData weapon : weapons) {
+            final WeaponButton weaponButton = new WeaponButton(weapon);
+
+            weaponsTable.add(weaponButton);
+        }
+
+        return weaponsTable;
     }
 
     public void closePopup () {
@@ -59,5 +82,15 @@ public class GameUI {
 
     public void dispose () {
         stage.dispose();
+    }
+
+    public static class WeaponButton extends IconButton {
+        public WeaponButton (final WeaponData data) {
+            super(Style.BLUE_40_35_7_13, "core/projectile_" + data.getBulletData().getBulletId().getName());
+
+            setOnClick(() -> {
+
+            });
+        }
     }
 }
