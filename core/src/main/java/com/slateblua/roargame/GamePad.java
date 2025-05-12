323ef8554e9c8d79430c4e21063628c1451ef816
Sergey Hayriyan <sega1ness@gmail.com>
2025-05-13 02:14:26 +0400

    Refactor GameUI

---
+++ core/src/main/java/com/slateblua/roargame/GamePad.java
@@ -0,0 +1,106 @@
+package com.slateblua.roargame;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.slateblua.roargame.scenes.components.OffsetButton;
+import com.slateblua.roargame.scenes.components.Style;
+
+public class GamePad extends Table {
+    private final Player playerInstance;
+    private final OffsetButton joystickKnobImage;
+
+    private final Vector2 knobRelativePosition = new Vector2(); // Knob's current position relative to joystick center
+    private final Vector2 knobVisualCenterOffset = new Vector2(); // To help draw knob centered
+
+    private final float joystickOperableRadius; // Max distance knob's center can travel from joystick's center
+    private final float joystickActivationRadius; // How far from center a touch must be to activate joystick
+
+    private boolean isDragging = false;
+
+    // Define sizes (adjust as needed)
+    private final float baseSize = 180f;
+
+    public GamePad () {
+        this.playerInstance = Player.get();
+
+        joystickKnobImage = new OffsetButton(Style.BLUE_40_35_7_13);
+
+        final float knobSize = 70f;
+        joystickOperableRadius = baseSize / 2f - knobSize / 2f;
+        joystickActivationRadius = baseSize / 2f;
+
+        knobVisualCenterOffset.set(knobSize / 2f, knobSize / 2f);
+
+        pad(30f);
+
+        final Table joystickStack = new Table();
+        joystickStack.add(joystickKnobImage); // Knob is drawn on top
+
+        this.add(joystickStack).size(baseSize, baseSize); // The joystick assembly size
+
+        resetKnobPosition();
+
+        joystickStack.addListener(new InputListener() {
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                Vector2 touchOffsetFromCenter = new Vector2(x - baseSize / 2f, y - baseSize / 2f);
+
+                if (touchOffsetFromCenter.len() <= joystickActivationRadius) {
+                    isDragging = true;
+                    updateJoystickState(touchOffsetFromCenter);
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void touchDragged (InputEvent event, float x, float y, int pointer) {
+                if (isDragging) {
+                    Vector2 touchOffsetFromCenter = new Vector2(x - baseSize / 2f, y - baseSize / 2f);
+                    updateJoystickState(touchOffsetFromCenter);
+                }
+            }
+
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                if (isDragging) {
+                    isDragging = false;
+                    resetKnobPosition();
+                    playerInstance.resetJoystickInfluence();
+                }
+            }
+        });
+    }
+
+    private void updateJoystickState (Vector2 touchOffsetFromCenter) {
+        knobRelativePosition.set(touchOffsetFromCenter);
+
+        if (knobRelativePosition.len() > joystickOperableRadius) {
+            knobRelativePosition.nor().scl(joystickOperableRadius);
+        }
+
+        float knobVisualX = (baseSize / 2f) + knobRelativePosition.x - knobVisualCenterOffset.x;
+        float knobVisualY = (baseSize / 2f) + knobRelativePosition.y - knobVisualCenterOffset.y;
+        joystickKnobImage.setPosition(knobVisualX, knobVisualY);
+
+        if (joystickOperableRadius == 0) return; // Avoid division by zero
+
+        float normalizedX = knobRelativePosition.x / joystickOperableRadius;
+        float normalizedY = knobRelativePosition.y / joystickOperableRadius;
+
+        playerInstance.joystickVelocityInfluence.set(
+            normalizedX * Player.SPEED,
+            normalizedY * Player.SPEED
+        );
+    }
+
+    private void resetKnobPosition () {
+        knobRelativePosition.set(0, 0);
+        float knobCenterX = baseSize / 2f - knobVisualCenterOffset.x;
+        float knobCenterY = baseSize / 2f - knobVisualCenterOffset.y;
+        joystickKnobImage.setPosition(knobCenterX, knobCenterY);
+    }
+
+}
--- core/src/main/java/com/slateblua/roargame/GameUI.java
+++ core/src/main/java/com/slateblua/roargame/GameUI.java
@@ -1,16 +1,10 @@
 package com.slateblua.roargame;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.slateblua.roargame.scenes.components.IconButton;
-import com.slateblua.roargame.scenes.components.OffsetButton;
-import com.slateblua.roargame.scenes.components.Style;
 import com.slateblua.roargame.scenes.pages.BasePage;
 import com.slateblua.roargame.scenes.pages.BasePopup;
 import com.slateblua.roargame.scenes.pages.NavController;
@@ -58,8 +52,7 @@ public class GameUI {
         final Array<WeaponData> weapons = Locator.get(GameData.class).getWeapons();
 
         for (WeaponData weapon : weapons) {
-            final WeaponButton weaponButton = new WeaponButton(weapon);
-
+            final WeaponButton weaponButton = new WeaponButton(weapon, data ->  Player.get().setCurrentWeapon(Locator.get(GameData.class).getWeaponMap().get(data)));
             weaponsTable.add(weaponButton);
         }
 
@@ -89,113 +82,5 @@ public class GameUI {
 
     public void dispose () {
         stage.dispose();
-    }
-
-    public static class GamePad extends Table {
-        private final Player playerInstance;
-        private final OffsetButton joystickKnobImage;
-
-        private final Vector2 knobRelativePosition = new Vector2(); // Knob's current position relative to joystick center
-        private final Vector2 knobVisualCenterOffset = new Vector2(); // To help draw knob centered
-
-        private final float joystickOperableRadius; // Max distance knob's center can travel from joystick's center
-        private final float joystickActivationRadius; // How far from center a touch must be to activate joystick
-
-        private boolean isDragging = false;
-
-        // Define sizes (adjust as needed)
-        private final float baseSize = 180f;
-
-        public GamePad() {
-            this.playerInstance = Player.get();
-
-            joystickKnobImage = new OffsetButton(Style.BLUE_40_35_7_13);
-
-            final float knobSize = 70f;
-            joystickOperableRadius = baseSize / 2f - knobSize / 2f;
-            joystickActivationRadius = baseSize / 2f;
-
-            knobVisualCenterOffset.set(knobSize / 2f, knobSize / 2f);
-
-            pad(30f);
-
-            final Table joystickStack = new Table();
-            joystickStack.add(joystickKnobImage); // Knob is drawn on top
-
-            this.add(joystickStack).size(baseSize, baseSize); // The joystick assembly size
-
-            resetKnobPosition();
-
-            joystickStack.addListener(new InputListener() {
-                @Override
-                public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                    Vector2 touchOffsetFromCenter = new Vector2(x - baseSize / 2f, y - baseSize / 2f);
-
-                    if (touchOffsetFromCenter.len() <= joystickActivationRadius) {
-                        isDragging = true;
-                        updateJoystickState(touchOffsetFromCenter);
-                        return true;
-                    }
-                    return false;
-                }
-
-                @Override
-                public void touchDragged(InputEvent event, float x, float y, int pointer) {
-                    if (isDragging) {
-                        Vector2 touchOffsetFromCenter = new Vector2(x - baseSize / 2f, y - baseSize / 2f);
-                        updateJoystickState(touchOffsetFromCenter);
-                    }
-                }
-
-                @Override
-                public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                    if (isDragging) {
-                        isDragging = false;
-                        resetKnobPosition();
-                        playerInstance.resetJoystickInfluence();
-                    }
-                }
-            });
-        }
-
-        private void updateJoystickState(Vector2 touchOffsetFromCenter) {
-            knobRelativePosition.set(touchOffsetFromCenter);
-
-            if (knobRelativePosition.len() > joystickOperableRadius) {
-                knobRelativePosition.nor().scl(joystickOperableRadius);
-            }
-
-            float knobVisualX = (baseSize / 2f) + knobRelativePosition.x - knobVisualCenterOffset.x;
-            float knobVisualY = (baseSize / 2f) + knobRelativePosition.y - knobVisualCenterOffset.y;
-            joystickKnobImage.setPosition(knobVisualX, knobVisualY);
-
-            if (joystickOperableRadius == 0) return; // Avoid division by zero
-
-            float normalizedX = knobRelativePosition.x / joystickOperableRadius;
-            float normalizedY = knobRelativePosition.y / joystickOperableRadius;
-
-            playerInstance.joystickVelocityInfluence.set(
-                normalizedX * Player.SPEED,
-                normalizedY * Player.SPEED
-            );
-        }
-
-        private void resetKnobPosition() {
-            knobRelativePosition.set(0, 0);
-            float knobCenterX = baseSize / 2f - knobVisualCenterOffset.x;
-            float knobCenterY = baseSize / 2f - knobVisualCenterOffset.y;
-            joystickKnobImage.setPosition(knobCenterX, knobCenterY);
-        }
-
-    }
-
-    public static class WeaponButton extends IconButton {
-        public WeaponButton (final WeaponData data) {
-            super(Style.BLUE_40_35_7_13, "core/projectile_" + data.getBulletData().getBulletId().getName());
-
-            setOnClick(() -> {
-                Player.get().setCurrentWeapon(Locator.get(GameData.class).getWeaponMap().get(data));
-            });
-        }
     }
 }
